2877ccab623eff420327884e44c8dcb0ccd00937
Joe Younes <joeyounes2@gmail.com>
2022-02-02 15:03:25 -0500

    correction au data_transformation

--- naive_methods.R
+++ naive_methods.R
@@ -8,13 +8,14 @@ source("./data_transformation.R")
 # Naive forecast draft
 
 #Creating timeseries
-sdate = data_agg$Date[1]
-edate = data_agg$Date[3652]
+sdate = data$Date[1]
+edate = data$Date[3652]
 data_agg_ts = ts(data_agg$Somme, start=sdate, end = edate, 
                  frequency=365.25)
 
-ffcast = data_agg$Date[2]
+ffcast = data$Date[2]
 naive_next_day = naive(data_agg_ts, h=1)
 
 forecast <- window(naive_next_day$fitted, start=ffcast) 
 observed <- window(naive_next_day$x, start=ffcast)
+
