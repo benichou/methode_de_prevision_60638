b16b70b33c8de29865608fceabb60698375988d0
FRANCK BENICHOU <franck.benichou@sciencespo.fr>
2022-02-02 23:10:45 -0500

    added structure for part on eda of target

---
+++ eda_target.R
@@ -0,0 +1,57 @@
+#
+# Program: eda_target.R
+#
+# Purpose: exploration of the target to check for trends, seasonality
+#  and special effects of week ends, stats holidays etc on 
+# daily electricity demand
+#
+# Written by: Team G, January 30 2021
+#
+# Updated: NA
+#          
+#         
+#          
+#         
+#          
+# ------------------------------------------------------
+
+
+
+# check for outlier values annual, monthly basis
+
+
+# check for seasonality annual monthly and weekly
+### all regions, separate regions
+
+
+## check for delta (growth or not) of electricity demand over time
+
+## check for overall trend over the overall period, 
+### all regions and each separate regions
+
+
+
+## analyze days when the military go out of their 3 bases in 
+### our region
+
+
+## check out analysis per weather type events, week end, 
+## statutory holidays, 
+
+
+
+
+
+## do the analysis to make the choice of traning, validation, and
+## test
+
+
+
+
+
+
+
+
+
+
+
