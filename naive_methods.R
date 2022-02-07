0baa079e6d8a66f9b75a031a24f5c555aa7c2246
Joe Younes <joeyounes2@gmail.com>
2022-02-07 15:41:35 -0500

    naive methods on validation

--- naive_methods.R
+++ naive_methods.R
@@ -22,43 +22,63 @@ library(timeSeries)
 source("./data_transformation.R")
 
 # Naive forecast draft
+data_training = data[732:1827,] #2014-2016
+data_validation = data[1828:2557,]  #2017-2018
+data_test = data[2558:3652,] #2019-2021
 
 #Creating timeseries
-sdate = 1
-data_ts = ts(data$SOMME, start=sdate, frequency=365.25)
+sdate = c(2017,1)
+data_val_ts = ts(data_validation$SOMME, start=sdate, frequency=365)
+
+#Random Walk
+set.seed(123)
+ffcast = c(2017,2)
+
+random_walk = rwf(data_val_ts, drift=TRUE, h=1)
+forecast_rw = window(random_walk$fitted, start=ffcast)
+observed_val <- window(random_walk$x, start=ffcast)
+
+print(accuracy(forecast_rw, observed_val)[,1:5])
+
 
 #Naive no change
-ffcast = 2
-naive_next_day = naive(data_ts, h=1)
-
+naive_next_day = naive(data_val_ts, h=1)
 forecast_next_day <- window(naive_next_day$fitted, start=ffcast) 
-observed <- window(naive_next_day$x, start=ffcast)
 
-#<<<<<<< HEAD
-#naif, naive seasonal 7 jours , moyenne mobile, random walk.
-#=======
-print(accuracy(forecast_next_day, observed)[,1:5])
+print(accuracy(forecast_next_day, observed_val)[,1:5])
 
 #Seasonal Naive, 7 jours
-data_ts_s <- ts(data$SOMME , start = sdate , frequency =365)
+val_ts_s <- ts(data_validation$SOMME , start = sdate, frequency = 7)
 
-
-ffcast_s = 8
-naive_s = snaive(data_ts_s, h=1)
+ffcast_s = c(2017,8)
+naive_s = snaive(val_ts_s, h=1)
 
 forecast_s <- window(naive_s$fitted, start=ffcast_s)
-observed_s <- window(naive_s$x , start=ffcast_s)
+observed_val_s <- window(naive_s$x , start=ffcast_s)
 
-print(accuracy(forecast_s, observed_s)[,1:5])
+print(accuracy(forecast_s, observed_val_s)[,1:5])
 
 #Moyenne mobile 3 jours
-ffcast_mm = 4
-naive_3days <- zoo::rollmean(data_ts, 3, align="right")
+ffcast_mm = c(2017,4)
+naive_3days <- zoo::rollmean(data_val_ts, 3, align="right")
 naive_mobile <- naive(naive_3days, h=1)
 
 forecast_mobile3 = window(naive_mobile$fitted, start=ffcast_mm)
-observed_mm <- window(data_ts , start = ffcast_mm)
+observed_val_mm <- window(data_val_ts , start = ffcast_mm)
 
-print(accuracy(forecast_mobile3, observed_mm)[,1:5])
+print(accuracy(forecast_mobile3, observed_val_mm)[,1:5])
 
-#>>>>>>> dev
+#ylim=c(min(naive_next_day$residuals, random_walk$residuals, naive_s$residuals, naive_mobile$residuals),max(naive_next_day$residuals, random_walk$residuals, naive_s$residuals, naive_mobile$residuals))
+
+plot(naive_next_day$residuals[1:58], lwd=2, type="o", ylim=c(-250000,200000), ylab= "Résidus (MW)", xlab="Jour", main = "Résidus des méthodes VS observations")
+#lines(random_walk$residuals[1:58], type="o", lwd=2, col="green")
+lines(naive_s$residuals[1:58], type="o",lwd=2, col="red")
+lines(naive_mobile$residuals[1:58], type="o",lwd=2, col="yellow")
+abline(0,0)
+legend(x=35, y=200000, legend=c("Naïve no change", "Naïve seasonal 7j", "Moyenne mobile 3j"), col=c("black", "red", "yellow"), lty=1, bg="light blue")
+    # plot(week.N, axes=F,
+    #      lty=1, type="l", ylim=c(min(week.N,week.T),max(week.N,week.T)),
+    #      ylab="Hourly demand (in MW) for Jan 3-9, 2010", xlab="")
+    # lines(week.T, lty=3, lwd=1.8)   
+     
+     
