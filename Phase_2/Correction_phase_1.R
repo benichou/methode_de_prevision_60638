5947bfbbcf6abb8ebf1bc14fdbc3f65feb6d40e7
Abderezzak Amimer <abderezzak.amimer@hec.ca>
2022-03-16 08:27:07 -0400

    auto.arima

--- Phase_2/Correction_phase_1.R
+++ Phase_2/Correction_phase_1.R
@@ -4,9 +4,74 @@
 # Importer le master df b??ti dans la premi??re phase
 load('master_df.Rdata')
 
+#ajouter du bruit dans les donn??es de validation et test pour 
+#les variables explicatives
 
-#Arranger l'effet de refroidissement pour ??liminer la portion
-#non lin??aire
+#data separation
+#beg_train <- 1 #2012/01/01
+#end_train <- 2192 #2017/12/31
+#end_valid <- 2922 #2019/12/31
+
+
+#https://www.nrel.gov/docs/fy12osti/56130.pdf
+set.seed(123)
+noise_wind <- c()
+for (i in seq(1 , length(data$AWND))) {
+  if (i < 2192) {
+    noise_wind[i] = 0
+  } else {
+    noise_wind[i] = rnorm(1 , mean = 0 , sd = 0.1187)
+  }
+}
+
+#https://www.forecastadvisor.com/detail/Texas/Dallas/75235/
+set.seed(123)
+noise_temp <- c()
+for (i in seq(1 , length(data$AWND))) {
+  if (i < 2192) {
+    noise_temp[i] = 0
+  } else {
+    noise_temp[i] = rnorm(1 , mean = 0 , sd = 3)
+  }
+}
+
+#didn't find any sources regarding that matter so we will
+#assume good prediction on relative humidity
+set.seed(123)
+noise_hum <- c()
+for (i in seq(1 , length(data$AWND))) {
+  if (i < 2192) {
+    noise_hum[i] = 0
+  } else {
+    noise_hum[i] = rnorm(1 , mean = 0 , sd = 5)
+  }
+}
+
+# adding the noise to our variables
+noisy_temp <- data$med_t + noise_temp
+noisy_hum <- data$RELATIVE_HUM_PERCENT + noise_hum
+noisy_wind <- data$AWND + noise_wind
+
+# creating the covariates with the noisy vars
+
+noisy_CDD <- c()
+for (i in seq(1 , length(noisy_temp))) {
+  noisy_CDD[i] <- max(noisy_temp[i] - 21 , 0 )
+}
+
+noisy_HDD <- c()
+for (i in seq(1 , length(noisy_temp))) {
+  noisy_HDD[i] <- max(16 - noisy_temp[i] , 0)
+} 
+
+data['noisy_CDD'] <- noisy_CDD
+
+data['noisy_HDD'] <- noisy_HDD
+
+
+
+#Arranger l'effet de refroidissement pour eliminer la portion
+#non lineaire
 temp_range <- seq(10 ,25 , 0.5)
 
 for (temp in temp_range) {
@@ -41,9 +106,22 @@ for (i in seq(1 , length(data$DATE))) {
 
 data['CP'] <- cp_final 
 
-#Humidit?? relative : Durant la premi??re phase, nous avons utilis?? 
-#l'humidit?? en fonction des diff??rent mois et nous avons obs??rv?? une 
-#am??lioration du nuage de point. Nous allons essayer de modifi??
+noisy_cp <- c()
+for (i in seq(1 , length(noisy_wind))) {
+  if (noisy_temp[i] < 16.0) {
+    noisy_cp[i]=round(((noisy_wind[i]**(1/2))*(16.00-noisy_temp[i])),
+                        digits = 4)
+  } else {
+    noisy_cp[i] = 0
+  }
+}
+
+data['noisy_cp'] <- noisy_cp
+
+
+#Humidite relative : Durant la premiere phase, nous avons utilise
+#l'humidite en fonction des different mois et nous avons observe une 
+#amelioration du nuage de point. Nous allons essayer de modifier
 
 
 dewpoint <- data$med_t-((100-data$RELATIVE_HUM_PERCENT)/5)+273.15
@@ -53,13 +131,13 @@ humidex <- data$med_t+
 
 
 #En observant le nuage de points, on remarque une forte 
-#ressemblance avec celui de la demande vs temp??rature.
+#ressemblance avec celui de la demande vs temperature.
 plot( x = humidex , y = data$SOMME , main = temp , 
       xlab = 'humidex' , 
       ylab = 'Demande' ,
       frame = FALSE)
 
-#La relation est trop lin??aire ce qui signifie que l'humidit?? seule
+#La relation est trop lineaire ce qui signifie que l'humidite seule
 #n'a pas d'apport avec la demande
 plot( x = humidex , y = data$med_t , main = temp , 
       xlab = 'humidex' , 
@@ -67,7 +145,7 @@ plot( x = humidex , y = data$med_t , main = temp ,
       frame = FALSE)
 
 
-#En utilisant une m??thode similaire ?? celle du refroidissement
+#En utilisant une methode similaire a celle du refroidissement
 temp_range <- seq(10 ,30 , 0.5)
 
 for (temp in temp_range) {
@@ -89,19 +167,32 @@ for (temp in temp_range) {
 
 #Le nuage de point indique que la meilleure relation est avec 22
 
-hum_final <- c()
+humidex_final <- c()
 for (i in seq(1 , length(data$DATE))) {
   if (data$med_t[i] > 22){
-    hum_final[i] = round(( (data$RELATIVE_HUM_PERCENT[i]**(1/2)) * 
+    humidex_final[i] = round(( (data$RELATIVE_HUM_PERCENT[i]**(1/2)) * 
                            (data$med_t[i] - 22) ),
                         digits = 4)
   } else {
-    hum_final[i] = 0
+    humidex_final[i] = 0
   }
 }
 
-data['hum_final'] <- hum_final
+data['humidex_final'] <- humidex_final
 
+
+noisy_humidex <- c()
+for (i in seq(1 , length(data$DATE))) {
+  if (noisy_temp[i] > 22){
+    noisy_humidex[i] = round(( (noisy_hum[i]**(1/2)) * 
+                                 (noisy_temp[i] - 22) ),
+                             digits = 4)
+  } else {
+    noisy_humidex[i] = 0
+  }
+}
+
+data['noisy_humidex'] <- noisy_humidex
 
 #marking the days arround holidays
 before_holiday <- rep(0 , length(data$DATE))
@@ -120,10 +211,42 @@ after_holiday[2] = 1
 data['before_holi'] <- before_holiday
 data['after_holiday'] <- after_holiday
 
+
+#Express the time with t starting at 1 to length of data to
+#make coding easier
+final_data['t'] <- seq(1 , length(final_data$DATE) , 1)
+
+#errors by quarter
+quarter <- c()
+for (i in seq(1 , 365)) {
+  if ( i <= 90) {
+    quarter[i] = 'Q1'
+  } else if ((i >= 91) & (i<=181) ) {
+    quarter[i] = 'Q2'
+  } else if ( (i>=182) & (i<=273)) {
+    quarter[i] = 'Q3'
+  } else {
+    quarter [i] = 'Q4'
+  }
+}
+
+quarter_2012 <- c('Q1' , quarter )
+quarter_2016 <- c('Q1' , quarter )
+quarter_2020 <- c('Q1' , quarter )
+quarter_2021 <- quarter[1:364]
+
+quarter <- c(quarter_2012 , quarter , quarter , quarter ,
+             quarter_2016 , quarter , quarter , quarter ,
+             quarter_2020 , quarter_2021) 
+
+data['quarter'] <- quarter
+
 #Enregistrer le df avec les variables utiles seulement
 colonnes_utiles <- c("DATE" , "SOMME" , "CDD" , "HDD" , "weekday" ,
-                     "CP" , "Holiday" , "hum_final" , "before_holi",
-                     "after_holiday")
+                     "CP" , "Holiday" , "humidex_final" , "before_holi",
+                     "after_holiday" , "noisy_humidex" , "noisy_cp",
+                     "noisy_HDD" , "noisy_CDD" , "quarter")
+
 # subset
 final_data <- data[colonnes_utiles]
 
--- Phase_2/regression.R
+++ Phase_2/regression.R
@@ -17,13 +17,13 @@ end_valid <- 2922 #2019/12/31
 
 rp <- 365
 
-#Express the time with t starting at 1 to length of data to
-#make coding easier
-final_data['t'] <- seq(1 , length(final_data$DATE) , 1)
+
 
 #-------------------------------------------------------------------
-# We can start with the basic two variables of interest
-# temp : CDD , HDD and weekday
+# We can start with the basic three variables of interest
+# CDD , HDD and weekday
+# This is just to prove that the lm models are not adequate
+# Due to the nature of their errors 
 
 model_1 <- lm( SOMME ~ CDD + HDD + factor(weekday) , 
                data = final_data , 
@@ -70,50 +70,132 @@ plot(x = seq(1 , length(model_1$model$SOMME)) ,
 #ACF of residuals
 acf(residuals(model_1))
 
-#looking at the performance measures
+#looking at the performance measures on prediction made 
+#on clean covariates
 
 #A : no retraining on all the validation
 pred_1_no <- predict(model_1 , 
-                    newdata = final_data[(end_train+1):end_valid,])
+                    newdata = final_data[(end_train+1):end_valid,] ,
+                    interval = 'prediction',
+                    level = c(0.95))
 
 #B : retraining with expending window
 pred_1_exp_1 <- predict(model_1_exp_1 , 
-                    newdata = final_data[(end_train+rp+1):end_valid,])
-pred_1_exp_1 <- c(pred_1_no[1:rp] , pred_1_exp_1)
+                  newdata = final_data[(end_train+rp+1):end_valid,] ,
+                  interval = 'prediction',
+                  level = c(0.95))
+
+pred_1_exp_1 <- rbind(pred_1_no[1:rp,] , pred_1_exp_1)
 
 #C : moving window
 pred_1_mov_1 <- predict(model_1_mov_1 ,
-                  newdata = final_data[(end_train+rp+1):end_valid,])
-pred_1_mov_1 <- c(pred_1_no[1:rp] , pred_1_mov_1)
+                  newdata = final_data[(end_train+rp+1):end_valid,],
+                  interval = 'prediction',
+                  level = c(0.95))
+
+pred_1_mov_1 <- rbind(pred_1_no[1:rp,] , pred_1_mov_1)
+
+#looking at the performance measures on prediction made 
+#on noisy covariates
+
 
 #accuracy check
-accuracy(pred_1_no , final_data[(end_train+1):end_valid,2])
-accuracy(pred_1_exp_1 , final_data[(end_train+1):end_valid,2])
-accuracy(pred_1_mov_1 , final_data[(end_train+1):end_valid,2])
+accuracy(pred_1_no[,1] , final_data[(end_train+1):end_valid,2])
+accuracy(pred_1_exp_1[,1] , final_data[(end_train+1):end_valid,2])
+accuracy(pred_1_mov_1[,1] , final_data[(end_train+1):end_valid,2])
+
+#couverture without retraining
+cv_no_1 <- c()
+for (i in seq(1, length(pred_1_no[,1]))){
+  if ((final_data$SOMME[(end_train+i)] >= pred_1_no[i , 2]) & 
+       (final_data$SOMME[(end_train+i)] <= pred_1_no[i , 3])){
+    cv_no_1[i] = 1
+  } else {
+    cv_no_1[i] = 0
+  }
+}
+print(mean(cv_no_1))
+
+#couverture with retraining expanding window
+cv_exp_1 <- c()
+for (i in seq(1, length(pred_1_exp_1[,1]))){
+  if ((final_data$SOMME[(end_train+i)] >= pred_1_exp_1[i , 2]) & 
+      (final_data$SOMME[(end_train+i)] <= pred_1_exp_1[i , 3])){
+    cv_exp_1[i] = 1
+  } else {
+    cv_exp_1[i] = 0
+  }
+}
+print(mean(cv_exp_1))
+
+#couverture with retraining moving window
+cv_mov_1 <- c()
+for (i in seq(1, length(pred_1_exp_1[,1]))){
+  if ((final_data$SOMME[(end_train+i)] >= pred_1_mov_1[i , 2]) & 
+      (final_data$SOMME[(end_train+i)] <= pred_1_mov_1[i , 3])){
+    cv_mov_1[i] = 1
+  } else {
+    cv_mov_1[i] = 0
+  }
+}
+print(mean(cv_mov_1))
+
+#La couvertue est supposée être de 95%. 
+#Visiblement, le fait que l'erreur est autocorrélée cause 
+#un problème même avec des variables explicatives sans bruits
+#Ajouter du bruit ne ferait qu'empirer les résultats.
+#On élimine donc ces modèles et on passe directement au erreur arma
 
 #--------------------------------------------------------------------
-#We should add holidays, before and after
-model_2 <- lm( SOMME ~ CDD + HDD + factor(weekday) + Holiday +
-                 before_holi + after_holiday, 
-               data = final_data , 
-               subset =  seq(beg_train , end_train))
+#Le modèle 2 portera sur les mêmes variables explicatives
+#que le modèle 1, toutefois, les erreurs seront, cette fois 
+#autocorrélées
 
-summary(model_2)
+#Dummy for the day covariate
+DMon <- ifelse(factor(final_data$weekday)=="Monday", 1, 0)
+DTue <- ifelse(factor(final_data$weekday)=="Tuesday", 1, 0)
+DWed <- ifelse(factor(final_data$weekday)=="Wednesday", 1, 0)
+DThu <- ifelse(factor(final_data$weekday)=="Thursday", 1, 0)
+DSat <- ifelse(factor(final_data$weekday)=="Saturday", 1, 0)
+DSun <- ifelse(factor(final_data$weekday)=="Sunday", 1, 0)
 
+#Making the demande a time series
+Yt <- timeSeries(final_data$SOMME , final_data$DATE)
+
+#binding our covariates as a matrix
+covariates_2 <- cbind(final_data$CDD , final_data$HDD ,
+                      DMon , DTue , DWed , DThu , DSat, DSun)
+
+#Fiting auto.arima
+model_2 <- auto.arima(Yt , xreg = covariates_2 )
+
+
+#Evaluating the residuals
+par(mfrow=c(1,1))
+
+#Autoccorelation des residus ?
+acf(residuals(model_2) , 
+    main ='Regression with ARIMA(1,1,3) errors')
+
+#Variance constante et esperance de 0 ?
+plot(x = model_2$fitted , y = model_2$residuals ,
+     xlab = 'Fitted values' , ylab = 'Residuals' ,
+     main = 'Diagnostic plot (model 2)')
+
+plot(x = seq(1,length(model_2$fitted)),
+     y = (model_2$residuals - mean(model_2$residuals))/sd(model_2$residuals),
+     xlab = 'Time' ,
+     ylab = 'Standerdized Residuals',
+     main = 'Standerdized residuals over time')
+
+#Normal ?
+qqnorm(model_2$residuals , main = 'Normal Q-Q')
+qqline(model_2$residuals )
+
+# 
 
 #--------------------------------------------------------------------
-#Looking at the relation between cp and HDD
-plot(x = final_data$CP , y = final_data$HDD ,
-     xlab = 'Effet de refroidissement' ,
-     ylab = 'Heating degree days')
 
-#Looking at the relation between humidex and CDD
-plot(x = final_data$hum_final , y = final_data$CDD ,
-     xlab = 'Effet de refroidissement' ,
-     ylab = 'Cooling degree days')
-
-#Because they are very similar, how important is it to have both CDD,
-#HDD and all the other variables
 
 #Turning off dev
 dev.off(dev.cur())
